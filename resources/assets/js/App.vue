ee399dcfad8bd1ed59313e46d86f683d320bc45d
al4er_shev <shevch19944@gmail.com>
2020-11-29 23:11:05 +0300

    +админка

---
+++ resources/assets/js/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+      <h2>Todods1</h2>
+      <hr>
+      <Auth/>
+  </div>
+</template>
+<script>
+    import Auth from './components/Auth'
+    export default {
+        components: {
+            Auth
+        },
+    }
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ resources/assets/js/components/Auth.vue
@@ -0,0 +1,39 @@
+<template>
+ <div>
+   <form class="login" @submit.prevent="login" id = "loginForm">
+     <h1>Sign in</h1>
+     <label>Login</label>
+     <input required type="text" v-model="userLogin" placeholder="Snoopy"/>
+     <label>Password</label>
+     <input required type="password" v-model="userPassword" placeholder="Password"/>
+     <input type="hidden" name="_token" value="Global.csrfToken">
+     <hr/>
+     <button type="submit">Login</button>
+   </form>
+ </div>
+</template>
+<script>
+    export default {
+      data(){
+          return{
+              userLogin: '',
+              userPassword: ''
+          }
+      },
+      methods:{
+          login(){
+              let loginForm = {
+                  userLogin: this.userLogin,
+                  userPassword: this.userPassword
+              }
+              loginForm = JSON.stringify(loginForm);
+              axios.post(location.origin+'/api/adminLogin/auth', loginForm)
+                .then( function(response) {
+                    console.log(response.data)
+                });
+              //console.log(loginForm);
+          }
+      }
+    }
+</script>
+    
