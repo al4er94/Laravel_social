d42968ec769ced62933c7d1a2e101464fc8af392
Shev <shevch19944@gmail.com>
2021-04-07 18:18:53 +0300

    Правки

--- database/migrations/2020_11_30_195754_add_shipped_via_and_terms_colums_to_purchase_orders_table.php
+++ database/migrations/2020_11_30_195754_add_shipped_via_and_terms_colums_to_purchase_orders_table.php
@@ -14,7 +14,7 @@ class AddShippedViaAndTermsColumsToPurchaseOrdersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('is_admin');
+            $table->string('is_admin')->default('0');
         });
     }
 
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -13,7 +13,6 @@ import VueRouter from 'vue-router'
 import router from './router'
 
 Vue.use(VueRouter);
-//console.log(router);
 new Vue({
   router,
   render: h => h(App),
